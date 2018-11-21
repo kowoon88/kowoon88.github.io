9e48d1c765df5e612c1dd124096f3d5e684dad5d
kowoon88 <43396295+kowoon88@users.noreply.github.com>
2018-11-20 23:04:39 -0500

    Update random.js

--- random.js
+++ random.js
@@ -1,14 +1,17 @@
 function setRandomColor() { 
     
     elements = document.getElementsByClassName("boxBlack");
+    
+    for (var i = 0; i < 100; i++) {
         
-    for (var i = 0; i < elements.length; i++) {
+        for (var j = 0; j < elements.length; j++) {
 
-        var x = Math.floor(Math.random() * 255);
-        var y = Math.floor(Math.random() * 255);
-        var z = Math.floor(Math.random() * 255);
-        var randomRGB = "rgb(" + x + ", " + y + ", " + z + ")";
-        elements[i].style.background = randomRGB;
+            var x = Math.floor(Math.random() * 255);
+            var y = Math.floor(Math.random() * 255);
+            var z = Math.floor(Math.random() * 255);
+            var randomRGB = "rgb(" + x + ", " + y + ", " + z + ")";
+            elements[i].style.background = randomRGB;
+        }
                 
     }
 
