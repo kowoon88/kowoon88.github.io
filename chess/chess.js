2c7400e32cd5158d4dd030e789519a45d74c8254
kowoon88 <43396295+kowoon88@users.noreply.github.com>
2018-11-24 20:55:01 -0500

    Update chess.js

--- chess/chess.js
+++ chess/chess.js
@@ -1,5 +1,38 @@
 function addPawn() { 
     
-    document.getElementById("pawn") = "<img src = 'pawn.png'>";
+    var pawn = document.getElementById("pawn");
+    pawn.style.visibility = "visible";
+    
+}
+
+function addRook() { 
+    
+    var rook = document.getElementById("rook");
+    rook.style.visibility = "visible";
+
+}
+
+function addBishop() { 
+    
+    var bishop = document.getElementById("bishop");
+    bishop.style.visibility = "visible";
+}
+
+function addKnight() { 
+    
+    var knight = document.getElementById("knight");
+    knight.style.visibility = "visible";
+}
+
+function addQueen() { 
+    
+    var queen = document.getElementById("queen");
+    queen.style.visibility = "visible"; 
+    
+}
+function addKing() { 
+    
+    var king = document.getElementById("king");
+    king.style.visibility = "visible";
     
 }
