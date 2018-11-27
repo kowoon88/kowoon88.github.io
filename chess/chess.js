03bf9e08a554306a8239823e605a2ab8dd5cdba1
kowoon88 <43396295+kowoon88@users.noreply.github.com>
2018-11-27 16:46:56 -0500

    Update chess.js

--- chess/chess.js
+++ chess/chess.js
@@ -1,38 +1,73 @@
 function addPawn() { 
     
     var pawn = document.getElementById("pawn");
-    pawn.style.visibility = "visible";
+    
+    if (pawn.style.visibility === "visible") {
+        pawn.style.visibility = "hidden";
+    }
+    else {
+        pawn.style.visibility = "visible";
+    }
     
 }
 
 function addRook() { 
     
     var rook = document.getElementById("rook");
-    rook.style.visibility = "visible";
+    if (rook.style.visibility === "visible") {
+        rook.style.visibility = "hidden";
+    }
+    else {
+        rook.style.visibility = "visible";
+    }
 
 }
 
 function addBishop() { 
     
     var bishop = document.getElementById("bishop");
-    bishop.style.visibility = "visible";
+   
+    if (bishop.style.visibility === "visible") {
+        bishop.style.visibility = "hidden";
+    }
+    else {
+        bishop.style.visibility = "visible";
+    }
 }
 
 function addKnight() { 
     
     var knight = document.getElementById("knight");
-    knight.style.visibility = "visible";
+    
+    if (knight.style.visibility === "visible") {
+        knight.style.visibility = "hidden";
+    }
+    else {
+        knight.style.visibility = "visible";
+    }
 }
 
 function addQueen() { 
     
     var queen = document.getElementById("queen");
-    queen.style.visibility = "visible"; 
+   
+    if (queen.style.visibility === "visible") {
+        queen.style.visibility = "hidden";
+    }
+    else {
+        queen.style.visibility = "visible";
+    }
     
 }
 function addKing() { 
     
     var king = document.getElementById("king");
-    king.style.visibility = "visible";
+    
+    if (king.style.visibility === "visible") {
+        king.style.visibility = "hidden";
+    }
+    else {
+        king.style.visibility = "visible";
+    }
     
 }
