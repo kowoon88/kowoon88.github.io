58b91ea98520529e0f23adcb5b0203dd2dd42ef9
kowoon88 <43396295+kowoon88@users.noreply.github.com>
2019-01-20 17:30:51 -0500

    Create chess.js

---
+++ chess.js
@@ -0,0 +1,73 @@
+function addPawn() { 
+    
+    var pawn = document.getElementById("pawn");
+    
+    if (pawn.style.visibility === "visible") {
+        pawn.style.visibility = "hidden";
+    }
+    else {
+        pawn.style.visibility = "visible";
+    }
+    
+}
+
+function addRook() { 
+    
+    var rook = document.getElementById("rook");
+    if (rook.style.visibility === "visible") {
+        rook.style.visibility = "hidden";
+    }
+    else {
+        rook.style.visibility = "visible";
+    }
+
+}
+
+function addBishop() { 
+    
+    var bishop = document.getElementById("bishop");
+   
+    if (bishop.style.visibility === "visible") {
+        bishop.style.visibility = "hidden";
+    }
+    else {
+        bishop.style.visibility = "visible";
+    }
+}
+
+function addKnight() { 
+    
+    var knight = document.getElementById("knight");
+    
+    if (knight.style.visibility === "visible") {
+        knight.style.visibility = "hidden";
+    }
+    else {
+        knight.style.visibility = "visible";
+    }
+}
+
+function addQueen() { 
+    
+    var queen = document.getElementById("queen");
+   
+    if (queen.style.visibility === "visible") {
+        queen.style.visibility = "hidden";
+    }
+    else {
+        queen.style.visibility = "visible";
+    }
+    
+}
+function addKing() { 
+    
+    var king = document.getElementById("king");
+    
+    if (king.style.visibility === "visible") {
+        king.style.visibility = "hidden";
+    }
+    else {
+        king.style.visibility = "visible";
+    }
+    
+}
